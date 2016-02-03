1a4d1eee11948bfda186965fb36713da11bfc270
Robert Del Favero <delf@drizzle.com>
2016-02-03 12:46:01 -0800

    Updated to use code from my older WifiApp1 project on personal VSO. This version binds straight to the WiFiAvailableNetwork objects, with simple text output.

--- WiFiScanner/WiFiScanner/MainPage.xaml.cs
+++ WiFiScanner/WiFiScanner/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.WiFi;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,52 @@ namespace WiFiScanner
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private static async System.Threading.Tasks.Task DoNetworkScanAsync(ListView populateList)
+        {
+            IReadOnlyList<WiFiAdapter> adapters = await WiFiAdapter.FindAllAdaptersAsync();
+
+            populateList.Items.Clear();
+
+            if (adapters.Count == 0)
+            {
+                populateList.Items.Add("No Wi-Fi adapters found.");
+            }
+            else
+            {
+                WiFiAdapter a = adapters[0];
+
+                await a.ScanAsync();
+                int numNetworksAfter = a.NetworkReport.AvailableNetworks.Count;
+
+                // Populate the ListView with a list of appropriate network APs.
+                if (numNetworksAfter > 0)
+                {
+                    // fill a list of valid network APs
+                    List<WiFiAvailableNetwork> displayList = new List<WiFiAvailableNetwork>();
+                    foreach (WiFiAvailableNetwork thisNetwork in a.NetworkReport.AvailableNetworks)
+                    {
+                        // skip hidden (no SSID) networks, skip Wi-Fi Direct devices, skip ad hoc 
+                        if ((thisNetwork.Ssid.Length != 0) && (thisNetwork.IsWiFiDirect == false) && (thisNetwork.NetworkKind == WiFiNetworkKind.Infrastructure))
+                        {
+                            displayList.Add(thisNetwork);
+                        }
+                    }
+
+                    // data bind that list to the ListView
+                    populateList.ItemsSource = displayList;
+                }
+            }
+        }
+
+        private async void ScanButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            await DoNetworkScanAsync(APList);
         }
     }
 }
